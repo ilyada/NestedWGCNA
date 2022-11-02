9c68f68de8296c936ae31410de9e9348d520d984
Ilya Dyugay <51997105+ilyada@users.noreply.github.com>
2022-11-02 13:29:04 +0300

    Corrected genfocus

--- GenFocus.py
+++ GenFocus.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from scipy.stats import zscore, spearmanr
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
@@ -9,17 +8,17 @@ def exclusion(lst1, lst2):
 def intersection(lst1, lst2): 
     return list(set(lst1) & set(lst2))
 
-def genfocus(df, focus_gene='PTPRC', tpm=True, spearman_thr=0.9, CVR_thr=0.6):
+def genfocus(df, focus_gene='PTPRC', method='spearman', tpm=True, corr_thr=0.9, CVR_thr=0.6):
     
     if not tpm:
-        ##создание таблицы TPM
+        ## convert FPKM to TPM
         df['Total']=df.sum(axis = 1, skipna = True)
         df_tpm = ((df.loc[:, df.columns != 'Total'].astype(float).div(df.Total.astype(float), axis=0))*(10**6)).fillna(0)
     else:
         df_tpm = df.copy()
     df_ings = df_tpm.copy()
     
-    #спирман
+    # finding genes correlated with focus gene
     if focus_gene=='eigengene':
         
         ss = StandardScaler()
@@ -28,32 +27,32 @@ def genfocus(df, focus_gene='PTPRC', tpm=True, spearman_thr=0.9, CVR_thr=0.6):
         pca = PCA(n_components=1)
         eigengene = pca.fit_transform(trans_df)
         eigengene = pd.Series(eigengene.T[0], index=df_tpm.index)
-        cor = df_tpm.corrwith(eigengene, axis=0, method='spearman')
+        cor = df_tpm.corrwith(eigengene, axis=0, method=method)
     else:
         assert focus_gene in df_tpm.columns, f'{focus_gene} is not in the table'
         cor = df_tpm.corrwith(df_tpm[focus_gene], axis=0, method='spearman')
         
-    df_ings.loc['spearman'] = cor
+    df_ings.loc['correlation'] = cor
 
-    ings = df_ings.loc[:, df_ings.loc['spearman',:]>spearman_thr].columns
+    ings = df_ings.loc[:, df_ings.loc['correlation',:]>corr_thr].columns
     print(f'INGS selected by spearman correlation: {len(ings)}')
     for j in [i for i in ings]:
         print(j, end='\t')
     if len(ings) == 1:
-        print(f'No genes correlate with {focus_gene} on correlation level {spearman_thr}')
+        print(f'No genes are correlate with {focus_gene} on correlation level {corr_thr}')
         return None
 
-    #раасчет Fings для каждого образца
+    # Fings calculation
     fings = df_ings.apply(lambda x: sum(x[ings])/len(ings), axis=1)
 
-    #нормализация
+    # Normalization
     df_ings_norm = df_ings.copy()
     df_ings_norm.loc[:, exclusion(df_tpm.columns,ings)] = df_ings_norm.loc[:, exclusion(df_tpm.columns,ings)].div(fings, axis=0)
     for j in ings:
         fing = df_ings.apply((lambda x: sum(x[exclusion(ings,j)])/(len(ings)-1)), axis=1)
         df_ings_norm.loc[:, j] = df_ings_norm.loc[:, j].div(fing, axis=0)
 
-    #вычисление CV
+    # CV computation
     for i in ings:
         if (df_ings.loc[df_tpm.index.to_list(),i].mean()==0) or (df_ings_norm.loc[df_tpm.index.to_list(),i].mean()==0):
             continue
@@ -65,7 +64,7 @@ def genfocus(df, focus_gene='PTPRC', tpm=True, spearman_thr=0.9, CVR_thr=0.6):
 
         df_ings_norm.loc['CVR',i] = df_ings_norm.loc['CV',i]/df_ings.loc['CV',i]
 
-    #определение окончательной выборки генов INGS
+    # Final INGS determination
     CVR_good = df_ings_norm.loc[:,df_ings_norm.loc['CVR',:]<CVR_thr].columns
     if len(CVR_good) == 0:
         return None
@@ -74,7 +73,7 @@ def genfocus(df, focus_gene='PTPRC', tpm=True, spearman_thr=0.9, CVR_thr=0.6):
     for j in [i for i in ings]:
         print(j, end='\t')
 
-    #финальная нормализация
+    # Final normalization
     fings = df_ings.apply((lambda x: sum(x[ings])/len(ings)), axis=1)
     
     df_tpm.loc[:, exclusion(df_tpm.columns,ings)] = df_tpm.loc[:, exclusion(df_tpm.columns,ings)].div(fings, axis=0)
